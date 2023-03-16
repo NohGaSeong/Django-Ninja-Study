55a900b2a42658c87e2dabb8478cba80e18f6d1f
yohan <noyohanx@naver.com>
2023-03-16 19:41:44 +0900

    파일 구조 변경

--- Ninja/urls.py
+++ Ninja/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from .api import api
+from django.urls import path, include
+from ..api.api import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", api.urls),
+    path("main/", include('main.urls'))
 ]
