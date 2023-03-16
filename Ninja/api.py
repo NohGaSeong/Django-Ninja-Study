d46745642b93ea5fb8ce30bfea57efa1348bb21a
yohan <noyohanx@naver.com>
2023-03-16 19:36:44 +0900

    Handling Responses

--- Ninja/api.py
+++ Ninja/api.py
@@ -2,6 +2,22 @@ from ninja import NinjaAPI, Schema
 
 api = NinjaAPI()
 
+
+class UserSchema(Schema):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+
+class Error(Schema):
+    message: str
+
+@api.get("/me", response={200: UserSchema, 403: Error})
+def me(request):
+    if not request.user.is_authenticated:
+        return 403, {"message": "Please sign in first"}
+    return request.user
+
 class HelloSchema(Schema):
     name:str = "world"
 
